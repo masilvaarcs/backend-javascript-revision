e81f58908430473ed93cc3b16159be8c7ff7242d
Marcos Silva <masilva.arcs@gmail.com>
2021-10-30 03:49:20 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,17 @@
+// tipo string
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+//Pesquisar sobre o que é: 
+//-- template string OU template literal
+
+
+// Concatenação (+)
+
+console.log(citacao + meuNome);
